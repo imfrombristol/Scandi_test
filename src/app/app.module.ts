8f4d113fba4383cb9e327bec564019ba3ba72245
Nick Del Nara <30962230+imfrombristol@users.noreply.github.com>
2018-01-12 19:08:43 +0200

    joke_second screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-
 
 import { AppComponent } from './app.component';
-import { List1Component } from './list1/list1.component';
-import { List2Component } from './list2/list2.component';
-import { BookmarkComponent } from './bookmark/bookmark.component';
-import { ProfileComponent } from './profile/profile.component';
-import { NotificationComponent } from './notification/notification.component';
+
+import { AppRoutingModule } from './app-routing.module';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    List1Component,
-    List2Component,
-    BookmarkComponent,
-    ProfileComponent,
-    NotificationComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([
-      {path: 'list1', component: List1Component},
-      {path: 'list2', component: List2Component},
-      {path: 'bookmark', component: BookmarkComponent},
-      {path: 'profile', component: ProfileComponent},
-      {path: 'notification', component: NotificationComponent},
-      {path: '', redirectTo: 'list1', pathMatch: 'full'}
-    ])
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
